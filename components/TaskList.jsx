8c8db35e023ed98d8eb11181205be6b8bf48e470
dd3dd <jacky81810@gmail.com>
2023-12-01 16:49:43 +0800

    aaa

--- components/TaskList.jsx
+++ components/TaskList.jsx
@@ -1,26 +1,32 @@
 import CheckComplete from "./CheckComplete";
 import Task from "./Task";
 
-const show = async () => {
+const show = async (currentPage = 1) => {
     try {
-        const res = await fetch('https://wayi.league-funny.com/api/task', {
+        const url = currentPage === 1
+            ? 'https://wayi.league-funny.com/api/task'
+            : `https://wayi.league-funny.com/api/task?page=${currentPage}`;
+        const res = await fetch(url, {
             cache: "no-store",
         });
-        if (!res.ok) {
-            throw new Error("Failed to fetch task");
+        const data = await res.json();
+
+        if (data.data.length > 0) {
+            const nextPageData = await show(currentPage + 1);
+            return [...data.data, ...nextPageData];
+        } else {
+            return [];
         }
-        return res.json();
     } catch (error) {
         console.log(error);
     }
 };
 export default async function TaskList() {
     const getData = await show();
-    const taskData = getData.data;
     return (
         <div className="mt-4">
             <CheckComplete />
-            {taskData.map(e =>
+            {getData.map(e =>
                 <Task key={e.id} id={e.id} name={e.name} description={e.description} is_completed={e.is_completed}
                     updated_at={e.updated_at}
                 />
